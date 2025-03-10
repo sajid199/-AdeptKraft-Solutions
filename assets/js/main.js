af872107ea180f6d1e448895982aac527cb90a5c
sajid199 <sajideee2015@gmail.com>
2025-03-10 13:02:32 +0530

    update index.html page

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,3 +243,12 @@ window.addEventListener('load', animateWorkflowItems);
   window.addEventListener('load', displayQuestions);
 
 })();
+// Smooth scrolling for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
